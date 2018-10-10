1ff1ec8b0218cfca6e764f9c8166eff0d61fe830
panncherry <maung.eainmatmay@gmail.com>
2018-10-09 20:26:57 -0400

    add User and Twitter classes

---
+++ twitter_alamofire_demo/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  twitter_alamofire_demo
+//
+//  Created by Pann Cherry on 10/9/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name: String?
+    
+    init(dictionary: [String : Any]) {
+        super.init()
+    }
+}
