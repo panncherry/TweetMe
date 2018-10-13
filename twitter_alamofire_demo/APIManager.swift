864e9ab7dca88cb19e6753326ab04fc9779b64d8
panncherry <maung.eainmatmay@gmail.com>
2018-10-12 23:23:02 -0400

    add func unreTweet

--- twitter_alamofire_demo/APIManager.swift
+++ twitter_alamofire_demo/APIManager.swift
@@ -165,6 +165,19 @@ class APIManager: SessionManager {
     }
     
     // MARK: TODO: Un-Retweet
+    func unretweet(_ tweet: Tweet, completion: @escaping (Tweet?, Error?) -> ()) {
+        let urlString = "https://api.twitter.com/1.1/statuses/unretweet.json"
+        let parameters = ["id": tweet.id]
+        request(urlString, method: .post, parameters: parameters, encoding: URLEncoding.queryString).validate().responseJSON { (response) in
+            if response.result.isSuccess,
+                let tweetDictionary = response.result.value as? [String: Any] {
+                let tweet = Tweet(dictionary: tweetDictionary)
+                completion(tweet, nil)
+            } else {
+                completion(nil, response.result.error)
+            }
+        }
+    }
     
     // MARK: TODO: Compose Tweet
     
